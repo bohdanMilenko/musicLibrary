9aae3d72c49dc6c508c020280bb5a4a578a9fca3
Bohdan Milenko <bogdan57milenko@gmail.com>
2020-01-19 18:40:22 -0500

    Refactored SQL Repo for Artist

--- src/com/musicLib/repository/SQLightRepository/ArtistsRepository.java
+++ src/com/musicLib/repository/SQLightRepository/ArtistsRepository.java
@@ -11,15 +11,24 @@ import java.util.List;
 import static com.musicLib.repository.SQLightRepository.MetaData.*;
 
 
-
 public class ArtistsRepository implements ArtistRepository {
 
     private PreparedStatement insertArtist;
-    private  PreparedStatement queryArtists;
+    private PreparedStatement queryArtists;
     private SessionManagerSQLite SessionManagerSQLite = new SessionManagerSQLite();
 
 
+    private static final String QUERY_ALL_ARTISTS = "SELECT " + COLUMN_ARTISTS_ID + "," + COLUMN_ARTISTS_NAME + " FROM " + TABLE_ARTISTS;
 
+    private static final String QUERY_ARTISTS_PREP = "SELECT " + COLUMN_ARTISTS_ID + " FROM " + TABLE_ARTISTS
+            + " WHERE " + COLUMN_ARTISTS_NAME + "= ?";
+
+    private static final String QUERY_ARTIST_BY_NAME = "SELECT " + COLUMN_ARTISTS_ID + " FROM " + TABLE_ARTISTS
+            + " WHERE " + COLUMN_ARTISTS_NAME + "= ?";
+
+
+    private static final String INSERT_ARTIST = "INSERT INTO " + TABLE_ARTISTS +
+            " (" + COLUMN_ARTISTS_NAME + ") VALUES(?)";
 
     @Override
     public boolean insert(com.musicLib.entities.Artist artist) {
@@ -28,31 +37,43 @@ public class ArtistsRepository implements ArtistRepository {
 
     @Override
     public List<Artist> queryAllArtists() {
-        List<com.musicLib.entities.Artist> artists = new ArrayList<>();
-        Connection conn;
-        Statement statement = null;
-        ResultSet resultSet = null;
-        try { conn = SessionManagerSQLite.getConnection();
-            System.out.println("In method");
-            statement = conn.createStatement();
-            resultSet = statement.executeQuery(QUERY_ARTISTS);
-            while (resultSet.next()) {
-                System.out.println("Inside");
-                Artist artist = new Artist();
-                artist.setId(resultSet.getInt(1));
-                artist.setName(resultSet.getString(2));
-                artists.add(artist);
-            }
-            return artists;
-        } catch (SQLException e) {
-            System.out.println("Query was not performed " + e.getMessage());
-            e.printStackTrace();
-            return artists;
-        }
+        ResultSet resultSet = getResultSet(QUERY_ALL_ARTISTS);
+        List<Artist> artists = resultSetToArtist(resultSet);
+        return artists;
     }
 
     @Override
-    public List<com.musicLib.entities.Artist> queryArtist(String artistName) {
+    public List<Artist> queryArtist(String artistName) {
+        PreparedStatement queryArtist = SessionManagerSQLite.getPreparedStatement(QUERY_ARTIST_BY_NAME);
+        List<Artist> returnList = new ArrayList<>();
+        try {
+            queryArtist.setString(1, artistName);
+            ResultSet rs = queryArtist.executeQuery();
+            returnList = resultSetToArtist(rs);
+            return returnList;
+        } catch (SQLException e) {
+            System.out.println(e + "\nCannot query by Artist Name");
+            return null;
+        }
+    }
+
+    private List<Artist> resultSetToArtist(ResultSet resultSet) {
+        List<Artist> artists = new ArrayList<>();
+        if (resultSet != null) {
+            try {
+                while (resultSet.next()) {
+                    Artist artist = new Artist();
+                    artist.setId(resultSet.getInt(1));
+                    artist.setName(resultSet.getString(2));
+                    artists.add(artist);
+                }
+                return artists;
+            } catch (SQLException e) {
+                System.out.println("Query was not performed " + e.getMessage());
+                e.printStackTrace();
+                return null;
+            }
+        }
         return null;
     }
 
@@ -62,59 +83,16 @@ public class ArtistsRepository implements ArtistRepository {
     }
 
 
-
-
-
-
-
-
-
-    private static final String INSERT_ARTIST = "INSERT INTO " + TABLE_ARTISTS +
-            " (" + COLUMN_ARTISTS_NAME + ") VALUES(?)";
-
-    private static final String QUERY_ARTISTS = "SELECT " + COLUMN_ARTISTS_ID + " FROM " + TABLE_ARTISTS
-            + " WHERE " + COLUMN_ARTISTS_NAME + "= ?";
-    private static final String QUERY_ARTISTS_PREP = "SELECT " + COLUMN_ARTISTS_ID + " FROM " + TABLE_ARTISTS
-            + " WHERE " + COLUMN_ARTISTS_NAME + "= ?";
-
-
-    public List<Artist> queryArtistsTable() {
+    private ResultSet getResultSet(String query) {
         List<Artist> artists = new ArrayList<>();
         Connection conn;
         Statement statement = null;
         ResultSet resultSet = null;
-        try { conn = SessionManagerSQLite.getConnection();
+        try {
+            conn = SessionManagerSQLite.getConnection();
             statement = conn.createStatement();
-            resultSet = statement.executeQuery(QUERY_ARTISTS);
-            while (resultSet.next()) {
-                Artist artist = new Artist();
-                artist.setId(resultSet.getInt(1));
-                artist.setName(resultSet.getString(2));
-                artists.add(artist);
-            }
-            return artists;
-        } catch (SQLException e) {
-            System.out.println("Query was not performed " + e.getMessage());
-            e.printStackTrace();
-            return artists;
-        }
-    }
-
-
-    public List<Artist> queryArtistsTry() {
-        List<Artist> artists2 = new ArrayList<>();
-        //No need to close as it is Try with resources;
-        try (Connection conn = SessionManagerSQLite.getConnection();
-             Statement statement = conn.createStatement();
-             ResultSet resultSet = statement.executeQuery("SELECT * FROM " + TABLE_ARTISTS);) {
-            while (resultSet.next()) {
-                Artist artist = new Artist();
-                artist.setId(resultSet.getInt(COLUMN_ARTISTS_ID));
-                artist.setName(resultSet.getString(COLUMN_ARTISTS_NAME));
-                artists2.add(artist);
-            }
-            return artists2;
-
+            resultSet = statement.executeQuery(QUERY_ALL_ARTISTS);
+            return resultSet;
         } catch (SQLException e) {
             System.out.println("Query was not performed " + e.getMessage());
             e.printStackTrace();
