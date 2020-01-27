f3e455ef4148ef9d9bc9c1206d47485ebaf6bf70
Bohdan Milenko <bogdan57milenko@gmail.com>
2020-01-26 21:38:54 -0500

    Added validation of objects for add and query methods

--- src/com/musicLib/services/AlbumService.java
+++ src/com/musicLib/services/AlbumService.java
@@ -2,14 +2,15 @@ package com.musicLib.services;
 
 import com.musicLib.entities.Album;
 import com.musicLib.exceptions.QueryException;
+import com.musicLib.exceptions.ServiceException;
 
 import java.util.List;
 
 public interface AlbumService {
 
-    public boolean add(Album album) throws QueryException;
+    public boolean add(Album album) throws QueryException, ServiceException;
 
-    public List<Album> getByArtistName(String artistName) throws QueryException;
+    public List<Album> getByArtistName(String artistName) throws QueryException, ServiceException;
 
     public List<Album> getByName(String albumName) throws QueryException;
 
--- src/com/musicLib/services/AlbumServiceImpl.java
+++ src/com/musicLib/services/AlbumServiceImpl.java
@@ -1,9 +1,10 @@
 package com.musicLib.services;
 
 import com.musicLib.entities.Album;
+import com.musicLib.entities.Artist;
 import com.musicLib.exceptions.ArtistNotFoundException;
-import com.musicLib.exceptions.DuplicatedRecordException;
 import com.musicLib.exceptions.QueryException;
+import com.musicLib.exceptions.ServiceException;
 import com.musicLib.repository.AlbumRepository;
 
 import java.sql.SQLException;
@@ -24,23 +25,49 @@ public class AlbumServiceImpl implements AlbumService {
     public AlbumServiceImpl() {
     }
 
-    public boolean add(Album album) throws QueryException {
+    public boolean add(Album album) throws ServiceException {
+        int artistID = getArtistID(album);
+        album = updateWithArtistID(album,artistID);
         try {
             return albumRepo.add(album);
-        } catch (ArtistNotFoundException e) {
-            throw new QueryException("Cannot find such artist in db", e);
-        } catch (DuplicatedRecordException e) {
-            throw new QueryException("Multiple albums with the same name, unable to define a correct one", e);
-        } catch (SQLException e) {
-            throw new QueryException("Issue with db connectivity", e);
+        }  catch (SQLException e) {
+            throw new ServiceException("Issue with adding album to db", e);
         }
     }
 
-    public List<Album> getByArtistName(String artistName) throws QueryException {
+    private int getArtistID(Album album) throws ServiceException {
+        return getArtistID(album.getName());
+    }
+
+    private int getArtistID(String album) throws ServiceException {
         try {
-            return albumRepo.queryAlbumsByArtistName(artistName);
+            return recordValidator.getAlbumID(album);
+        }catch (QueryException e){
+            throw new ServiceException("Cannot get artist ID", e);
+        }
+    }
+
+    private Album updateWithArtistID(Album album, int artistID){
+        Artist artist = album.getArtist();
+        artist.setId(artistID);
+        album.setArtist(artist);
+        return album;
+    }
+
+    public List<Album> getByArtistName(String artistName) throws ServiceException {
+        int artistID = getArtistIDByArtistName(artistName);
+        try {
+            return albumRepo.queryAlbumsByArtistName(artistID);
         } catch (SQLException e) {
-            throw new QueryException("Issue with db connectivity", e);
+            throw new QueryException("Unable to query albums by artist name", e);
+        }
+    }
+
+    private int getArtistIDByArtistName(String artistName) throws ServiceException{
+        try{
+            return recordValidator.getArtistID(artistName);
+        }catch (QueryException e){
+            throw new ServiceException("Cannot get artist ID by artist name", e);
         }
     }
 
