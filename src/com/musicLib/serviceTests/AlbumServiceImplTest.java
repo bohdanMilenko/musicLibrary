1a8cf2243b4d352f7e98010cbddc5e7aab42e55a
Bohdan Milenko <bogdan57milenko@gmail.com>
2020-02-04 21:42:56 -0500

    Added tests for Albums

---
+++ src/com/musicLib/serviceTests/AlbumServiceImplTest.java
@@ -0,0 +1,15 @@
+package com.musicLib.serviceTests;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class AlbumServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void add() {
+    }
+}
