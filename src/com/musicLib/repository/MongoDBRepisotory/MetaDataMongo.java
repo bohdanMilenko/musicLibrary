1b9299c0e3b6a1a13c0c2d086d9458e19e902976
Bohdan Milenko <bogdan57milenko@gmail.com>
2020-02-05 23:05:44 -0500

    New variables

--- src/com/musicLib/repository/MongoDBRepisotory/MetaDataMongo.java
+++ src/com/musicLib/repository/MongoDBRepisotory/MetaDataMongo.java
@@ -2,14 +2,17 @@ package com.musicLib.repository.MongoDBRepisotory;
 
 public class MetaDataMongo {
 
-    public static final String ARTIST_NAME = "artistName";
-    public static final String ARTIST_YEAR_FOUNDED = "artistFounded";
-    public static final String ARTIST_GENRE = "artistGenre";
-    public static final String ARTIST_ALBUMS = "albumList";
-    public static final String ALBUM_NAME = ARTIST_ALBUMS + ".albumName";
-    public static final String ALBUM_SONGS_NUMBER = ARTIST_ALBUMS + ".albumSongsNumber";
-    public static final String ALBUM_YEAR_RELEASED = ARTIST_ALBUMS +".albumYearReleased";
-    public static final String ALBUM_SONGS = ARTIST_ALBUMS + ".songsList";
-    public static final String SONGS_NAME = ALBUM_SONGS + ".songName";
-    public static final String SONGS_LENGTH = ALBUM_SONGS +".songLength";
+    static final String SONGS_COLLECTION = "songs";
+    static final String ARTISTS_COLLECTION = "artists";
+
+    static final String ID = "_id";
+    static final String ARTIST_NAME = "artistName";
+    static final String ARTIST_YEAR_FOUNDED = "artistFounded";
+    static final String ARTIST_ALBUMS = "albumList";
+    static final String ALBUM_NAME = ARTIST_ALBUMS + ".albumName";
+    static final String ALBUM_SONGS_NUMBER = ARTIST_ALBUMS + ".albumSongsNumber";
+    static final String ALBUM_YEAR_RELEASED = ARTIST_ALBUMS + ".albumYearReleased";
+    static final String ALBUM_SONGS = ARTIST_ALBUMS + ".songsList";
+    static final String SONGS_NAME = ALBUM_SONGS + ".songName";
+    static final String SONGS_LENGTH = ALBUM_SONGS + ".songLength";
 }
