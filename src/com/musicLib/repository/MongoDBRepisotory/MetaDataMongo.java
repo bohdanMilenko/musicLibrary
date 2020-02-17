b0830e9b40689277a7f4e2e9bdb96cfeb449919d
Bohdan Milenko <bogdan57milenko@gmail.com>
2020-02-17 15:28:06 -0500

    Changed the db Schema and implemented getNextSequence()

--- src/com/musicLib/repository/MongoDBRepisotory/MetaDataMongo.java
+++ src/com/musicLib/repository/MongoDBRepisotory/MetaDataMongo.java
@@ -1,32 +1,57 @@
 package com.musicLib.repository.MongoDBRepisotory;
 
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.FindOneAndUpdateOptions;
+import com.mongodb.client.model.ReturnDocument;
+import org.bson.Document;
+
 public class MetaDataMongo {
 
-    static final String ALBUMS_COLLECTION = "albums";
     static final String ARTISTS_COLLECTION = "artists";
+    static final String ALBUMS_COLLECTION = "albums";
+    static final String SONGS_COLLECTION = "songs";
+
 
     static final String ARTIST_ID = "_id";
     static final String ARTIST_NAME = "artistName";
-    static final String ARTIST_YEAR_FOUNDED = "artistFounded";
     static final String ARTIST_ALBUMS_LIST = "albumList";
-
-    static final String ARTIST_ALBUM_ID = ARTIST_ALBUMS_LIST + "._id";
-    static final String ARTIST_ALBUM_NAME = ARTIST_ALBUMS_LIST + ".albumName";
 
 
     static final String ALBUM_ID = "_id";
     static final String ALBUM_NAME = "albumName";
-    static final String ALBUM_SONGS_LIST = "songsList";
-    static final String SONG_NAME = ALBUM_SONGS_LIST + ".songName";
-    static final String SONG_NUMBER = ALBUM_SONGS_LIST + ".trackNumber";
+    static final String ALBUM_ARTIST_INFO = "artistInfo";
+    static final String ALBUM_ARTIST_ID = ALBUM_ARTIST_INFO + "._id";
+    static final String ALBUM_ARTIST_NAME = ALBUM_ARTIST_INFO + ".artistName";
+
+    
+
+    static final String SONG_ID = "_id";
+    static final String SONG_NAME = "songName";
+    static final String SONG_TRACK_NUMBER = "trackNumber";
+    static final String SONG_ALBUM_INFO = "albumInfo";
+    static final String SONG_ALBUM_ID = SONG_ALBUM_INFO + "._id";
+    static final String SONG_ALBUM_NAME = SONG_ALBUM_INFO +".albumName";
+    static final String SONG_ARTIST_INFO = "artistInfo";
+    static final String SONG_ARTIST_ID = SONG_ARTIST_INFO + "._id";
+    static final String SONG_ARTIST_NAME = SONG_ARTIST_INFO +".artistName";
+
+
+    static final String ID_PARAM = "idParam";
+    static final String ID_SEQ = "seq";
+
+    static int getNextSequence(MongoCollection<Document> collection){
+        Document findRequest = new Document();
+        findRequest.append("_id", ID_PARAM);
+        Document updateRequest =  new Document();
+        updateRequest.append("$inc", new Document(ID_SEQ,1));
+        FindOneAndUpdateOptions options = new FindOneAndUpdateOptions();
+        options.upsert(true);
+        options.returnDocument(ReturnDocument.AFTER);
+        Document returnDoc =  collection.findOneAndUpdate(findRequest,updateRequest,options);
+        return (int) returnDoc.get(ID_SEQ);
+    }
 
 
 
-
-
-    static final String ALBUM_SONGS_NUMBER = ARTIST_ALBUMS_LIST + ".albumSongsNumber";
-    static final String ALBUM_YEAR_RELEASED = ARTIST_ALBUMS_LIST + ".albumYearReleased";
-    static final String ALBUM_SONGS = ARTIST_ALBUMS_LIST + ".songsList";
-    static final String SONGS_NAME = ALBUM_SONGS + ".songName";
-    static final String SONGS_LENGTH = ALBUM_SONGS + ".songLength";
+    
 }
