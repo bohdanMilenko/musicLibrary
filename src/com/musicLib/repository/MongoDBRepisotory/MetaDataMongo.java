f8b1c973a5163aea562e0cbc16175e0a8a0be697
Bohdan Milenko <bogdan57milenko@gmail.com>
2020-02-17 22:56:11 -0500

    Changed naming for fields in MongoDB

--- src/com/musicLib/repository/MongoDBRepisotory/MetaDataMongo.java
+++ src/com/musicLib/repository/MongoDBRepisotory/MetaDataMongo.java
@@ -20,38 +20,35 @@ public class MetaDataMongo {
     static final String ALBUM_ID = "_id";
     static final String ALBUM_NAME = "albumName";
     static final String ALBUM_ARTIST_INFO = "artistInfo";
-    static final String ALBUM_ARTIST_ID = ALBUM_ARTIST_INFO + "._id";
-    static final String ALBUM_ARTIST_NAME = ALBUM_ARTIST_INFO + ".artistName";
+    static final String ALBUM_ARTIST_ID = ALBUM_ARTIST_INFO + "." + ARTIST_ID;
+        static final String ALBUM_ARTIST_NAME = ALBUM_ARTIST_INFO + "." + ARTIST_NAME;
 
-    
 
     static final String SONG_ID = "_id";
     static final String SONG_NAME = "songName";
     static final String SONG_TRACK_NUMBER = "trackNumber";
     static final String SONG_ALBUM_INFO = "albumInfo";
-    static final String SONG_ALBUM_ID = SONG_ALBUM_INFO + "._id";
-    static final String SONG_ALBUM_NAME = SONG_ALBUM_INFO +".albumName";
+    static final String SONG_ALBUM_ID = SONG_ALBUM_INFO + "." + ALBUM_ID;
+    static final String SONG_ALBUM_NAME = SONG_ALBUM_INFO + "." + ALBUM_NAME;
     static final String SONG_ARTIST_INFO = "artistInfo";
-    static final String SONG_ARTIST_ID = SONG_ARTIST_INFO + "._id";
-    static final String SONG_ARTIST_NAME = SONG_ARTIST_INFO +".artistName";
+    static final String SONG_ARTIST_ID = SONG_ARTIST_INFO + "." + ARTIST_ID;
+    static final String SONG_ARTIST_NAME = SONG_ARTIST_INFO + "." + ARTIST_NAME;
 
 
     static final String ID_PARAM = "idParam";
     static final String ID_SEQ = "seq";
 
-    static int getNextSequence(MongoCollection<Document> collection){
+    static int getNextSequence(MongoCollection<Document> collection) {
         Document findRequest = new Document();
         findRequest.append("_id", ID_PARAM);
-        Document updateRequest =  new Document();
-        updateRequest.append("$inc", new Document(ID_SEQ,1));
+        Document updateRequest = new Document();
+        updateRequest.append("$inc", new Document(ID_SEQ, 1));
         FindOneAndUpdateOptions options = new FindOneAndUpdateOptions();
         options.upsert(true);
         options.returnDocument(ReturnDocument.AFTER);
-        Document returnDoc =  collection.findOneAndUpdate(findRequest,updateRequest,options);
+        Document returnDoc = collection.findOneAndUpdate(findRequest, updateRequest, options);
         return (int) returnDoc.get(ID_SEQ);
     }
 
 
-
-    
 }
