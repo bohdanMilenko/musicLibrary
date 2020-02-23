5db941333869c9a55023a9613f6f1f43bec4e42c
Bohdan Milenko <bogdan57milenko@gmail.com>
2020-02-23 18:02:19 -0500

    Added testing for all services for SQLite3

--- src/com/musicLib/ApplicationMain.java
+++ src/com/musicLib/ApplicationMain.java
@@ -1,6 +1,9 @@
 package com.musicLib;
 
+import com.musicLib.entities.Album;
+import com.musicLib.entities.Artist;
 import com.musicLib.entities.Song;
+import com.musicLib.exceptions.ServiceException;
 import com.musicLib.repository.AlbumRepository;
 import com.musicLib.repository.ArtistRepository;
 import com.musicLib.repository.SQLightRepository.AlbumRepositorySQL;
@@ -10,16 +13,81 @@ import com.musicLib.repository.SongRepository;
 import com.musicLib.services.*;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ApplicationMain {
 
-    public static void main(String[] args) {
+    static SongService songServiceSQL;
+    static AlbumService albumServiceSQL;
+    static ArtistService artistServiceSQL;
 
-        SongService songServiceSQL;
-        AlbumService albumServiceSQL;
-        ArtistService artistServiceSQL;
+    static ArtistRepositorySQL artistRepositorySQLite;
+    static AlbumRepository albumRepositorySQLite;
+    static SongRepository songRepositorySQLite;
 
+    public static void main(String[] args) throws ServiceException, SQLException {
+
+        initializeServices();
+
+        Artist artistDrake = new Artist();
+        artistDrake.setName("Drake");
+        Album albumDrake = new Album();
+        albumDrake.setName("Scorpion");
+        albumDrake.setArtist(artistDrake);
+
+        Artist artistLedZeppelin = new Artist();
+        artistLedZeppelin.setName("Led Zeppelin");
+        Album albumGoingToCalifornia = new Album();
+        albumGoingToCalifornia.setName("Going To California 4");
+        albumGoingToCalifornia.setArtist(artistLedZeppelin);
+
+        Song songLedZeppelin = new Song();
+        songLedZeppelin.setArtist(artistLedZeppelin);
+        songLedZeppelin.setAlbum(albumGoingToCalifornia);
+        songLedZeppelin.setName("Black Dog");
+
+        artistServiceSQL.add(artistDrake);
+        artistServiceSQL.add(artistLedZeppelin);
+
+        List<Artist> artistList = artistServiceSQL.getByName(artistDrake);
+        artistList.forEach(v -> System.out.println(v.getName()));
+
+        List<Artist> artistList2 = artistServiceSQL.getAll();
+        artistList2.forEach(v -> System.out.println(v.getName()));
+
+        artistServiceSQL.delete(artistDrake);
+        artistServiceSQL.delete(artistLedZeppelin);
+
+
+//      TESTING ALBUM SERVICE FOR SQLITE
+        albumServiceSQL.add(albumDrake);
+        albumServiceSQL.add(albumGoingToCalifornia);
+        List<Album> albumList = albumServiceSQL.getByArtist(artistDrake);
+        albumList.forEach(v -> System.out.println(v.getName()));
+
+        List<Album> albumList2 = albumServiceSQL.get(albumGoingToCalifornia);
+        albumList2.forEach(v -> System.out.println(v.getName()));
+
+        albumServiceSQL.delete(albumGoingToCalifornia);
+        albumServiceSQL.deleteAlbumsFromArtist(artistDrake);
+
+
+
+//      TESTING SONGS SERVICE FOR SQLITE
+        songServiceSQL.add(songLedZeppelin);
+        List<Song> foundSongs = songServiceSQL.get(songLedZeppelin);
+        foundSongs.forEach(v -> System.out.println(v.toString()));
+
+        List<Song> foundSongs2 = songServiceSQL.getByAlbum(albumGoingToCalifornia);
+        foundSongs2.forEach(v -> System.out.println(v.toString()));
+
+        songServiceSQL.delete(songLedZeppelin);
+
+
+    }
+
+    private static void initializeServices() {
         ArtistRepository artistRepositorySQLite = new ArtistRepositorySQL();
         AlbumRepository albumRepositorySQLite = new AlbumRepositorySQL();
         SongRepository songRepositorySQLite = new SongRepositorySQL();
@@ -39,57 +107,6 @@ public class ApplicationMain {
         albumServiceSQL.setSongService(songServiceSQL);
         albumServiceSQL.setArtistService(artistServiceSQL);
         albumServiceSQL.setRecordValidator(recordValidator);
-
-
-        try {
-            List<Song> songs = songRepositorySQLite.getByAlbumId(5);
-            songs.forEach(v -> System.out.println(v.getName()));
-        } catch (
-                SQLException e) {
-            System.out.println(e.getMessage());
-        }
-
-//
-//        List<Artist> query = artistServiceImpl.getAll();
-//
-//        //query.forEach(v-> System.out.println(v.getName()));
-//
-//        AlbumRepository albumRepository = new AlbumRepositorySQL();
-//        SongRepository songRepositorySQL = new SongRepositorySQL();
-//
-//        List<Album> albumList = new ArrayList<>();
-//        albumList = albumServiceImpl.getByName("Smth");
-//
-//        Artist drake = new Artist();
-//        drake.setName("Drake");
-//        Album albumDrake = new Album();
-//        albumDrake.setName("Scorpion");
-//        albumDrake.setArtist(drake);
-//        System.out.println(albumServiceImpl.add(albumDrake));
-//
-//        albumList.forEach(v -> System.out.println(v.getName()));
-//
-//        Artist ledZeppelin = new Artist();
-//        drake.setName("Led Zeppelin");
-//        Album albumGoingToCalifornia = new Album();
-//        albumGoingToCalifornia.setName("Going To California 4");
-//        albumGoingToCalifornia.setArtist(ledZeppelin);
-//        System.out.println(albumServiceImpl.add(albumGoingToCalifornia));
-//
-//        artistServiceImpl.delete("Kendrick Lamar");
-//
-//        SongService songService = new SongServiceImpl();
-//        List<Song> foundSongs = songService.getByName("Flaming Telepaths");
-//        foundSongs.forEach(v -> System.out.println(v.getName()));
-//        Artist tempArtist = foundSongs.get(0).getArtist();
-//        Album tempAlbum = foundSongs.get(0).getAlbum();
-//
-//        System.out.println(tempArtist.toString());
-//        System.out.println(tempAlbum.toString());
-//
-//        System.out.println(artistServiceImpl.delete("Blue Öyster Cult"));
-
-
     }
 
 
