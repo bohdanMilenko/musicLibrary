e46691e2d3e209cabd0c3c623527adbb19c3535d
Bohdan Milenko <bogdan57milenko@gmail.com>
2020-02-22 19:27:16 -0500

    Finalized MetaData for Songs Repo Mongo

--- src/com/musicLib/repository/MongoDBRepisotory/MetaDataMongo.java
+++ src/com/musicLib/repository/MongoDBRepisotory/MetaDataMongo.java
@@ -22,20 +22,20 @@ public class MetaDataMongo {
     static final String ALBUM_ID = "_id";
     static final String ALBUM_NAME = "albumName";
     static final String ALBUM_ARTIST_INFO = "artistInfo";
-    static final String ALBUM_INSERT_ARTIST_ID = ARTIST_ID;
-    static final String ALBUM_INSERT_ARTIST_NAME = ARTIST_NAME;
     static final String ALBUM_QUERY_ARTIST_ID = ALBUM_ARTIST_INFO + "." + ARTIST_ID;
-    static final String ALBUM_QUERY_ARTIST_NAME =ALBUM_ARTIST_INFO + "." +ARTIST_NAME;
+    static final String ALBUM_QUERY_ARTIST_NAME = ALBUM_ARTIST_INFO + "." + ARTIST_NAME;
 
     static final String SONG_ID = "_id";
     static final String SONG_NAME = "songName";
     static final String SONG_TRACK_NUMBER = "trackNumber";
+
     static final String SONG_ALBUM_INFO = "albumInfo";
-    static final String SONG_ALBUM_ID = SONG_ALBUM_INFO + "." + ALBUM_ID;
-    static final String SONG_ALBUM_NAME = SONG_ALBUM_INFO + "." + ALBUM_NAME;
+    static final String SONG_QUERY_ALBUM_ID = SONG_ALBUM_INFO + "." + ALBUM_ID;
+    static final String SONG_QUERY_ALBUM_NAME = SONG_ALBUM_INFO + "." + ALBUM_NAME;
+
     static final String SONG_ARTIST_INFO = "artistInfo";
-    static final String SONG_ARTIST_ID = SONG_ARTIST_INFO + "." + ARTIST_ID;
-    static final String SONG_ARTIST_NAME = SONG_ARTIST_INFO + "." + ARTIST_NAME;
+    static final String SONG_QUERY_ARTIST_ID = SONG_ARTIST_INFO + "." + ARTIST_ID;
+    static final String SONG_QUERY_ARTIST_NAME = SONG_ARTIST_INFO + "." + ARTIST_NAME;
 
 
     static final String ID_PARAM = "idParam";
