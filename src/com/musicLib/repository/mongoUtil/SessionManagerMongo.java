c617024e132d78e5b10689754c0f994e0a7fefb3
Bohdan Milenko <bogdan57milenko@gmail.com>
2020-03-11 22:58:46 -0400

    New Functionality to connect to mongo server running on Docker

--- src/com/musicLib/repository/mongoUtil/SessionManagerMongo.java
+++ src/com/musicLib/repository/mongoUtil/SessionManagerMongo.java
@@ -1,36 +1,78 @@
 package com.musicLib.repository.mongoUtil;
 
 import com.mongodb.MongoClient;
-import com.mongodb.MongoClientURI;
 import com.mongodb.client.MongoDatabase;
+import com.musicLib.exceptions.DbConnectionException;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 public class SessionManagerMongo {
 
-    private static final MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://localhost:27017"));
-    private static final String databaseName = loadMongoProperties();
+    private static final String databaseNameProperty = "dbName";
+    private static final String dockerIpProperty = "ip";
+    private static final String mongoPortExternalProperty = "externalPort";
+    private static final String mongoUserNameProperty = "MONGO_INITDB_ROOT_USERNAME";
+    private static final String mongoPasswordProperty = "MONGO_INITDB_ROOT_PASSWORD";
 
-    public static MongoClient getMongoClient(){
-        return mongoClient;
+    private static Map<String, String> properties;
+
+    static {
+        try {
+            properties = loadMongoProperties();
+        } catch (DbConnectionException e) {
+            e.printStackTrace();
+        }
     }
-
-    public static MongoDatabase getDbFromPropertyFile(){
-        return mongoClient.getDatabase(databaseName);
-    }
-
-    private static String loadMongoProperties() {
-        String returnString = "";
+    private static Map<String, String> loadMongoProperties() throws DbConnectionException {
+        Map<String, String> returnMap = new HashMap<>();
         try (FileInputStream inputStream = new FileInputStream("connectionMongo.property")) {
             Properties p = new Properties();
             p.load(inputStream);
-            returnString = p.getProperty("dbName");
+            returnMap.put(databaseNameProperty, p.getProperty(databaseNameProperty));
+            returnMap.put(dockerIpProperty, p.getProperty(dockerIpProperty));
+            returnMap.put(mongoPortExternalProperty, p.getProperty(mongoPortExternalProperty));
+            returnMap.put(mongoUserNameProperty, p.getProperty(mongoUserNameProperty));
+            returnMap.put(mongoPasswordProperty, p.getProperty(mongoPasswordProperty));
+            return returnMap;
         } catch (IOException e) {
-            System.out.println(e.getMessage()+ ": Cannot load Mongo Properties");
+            System.out.println(e.getMessage() + ": Cannot load Mongo Properties");
             e.printStackTrace();
+            throw new DbConnectionException("Unable to connect to MongoDB", e);
         }
-        return returnString;
     }
+
+    private static final String databaseName = properties.get(databaseNameProperty);
+    private static final String ip = properties.get(dockerIpProperty);
+    private static final String externalPort = properties.get(mongoPortExternalProperty);
+    private static final String mongoUser = properties.get(mongoUserNameProperty);
+    private static final String mongoPassword = properties.get(mongoPasswordProperty);
+
+
+    private static final MongoClient mongoClient = new MongoClient();
+
+    public static MongoClient getMongoClient() {
+        return mongoClient;
+    }
+
+    public static MongoDatabase getDbFromPropertyFile() {
+        return mongoClient.getDatabase(databaseName);
+    }
+
+//    private static String loadMongoProperties() {
+//        String returnString = "";
+//        try (FileInputStream inputStream = new FileInputStream("connectionMongo.property")) {
+//            Properties p = new Properties();
+//            p.load(inputStream);
+//            returnString = p.getProperty("dbName");
+//
+//        } catch (IOException e) {
+//            System.out.println(e.getMessage()+ ": Cannot load Mongo Properties");
+//            e.printStackTrace();
+//        }
+//        return returnString;
+//    }
 }
