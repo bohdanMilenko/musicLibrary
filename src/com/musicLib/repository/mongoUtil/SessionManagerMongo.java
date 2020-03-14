f8a6ba22f485487ba45e702cae184a2f4c03657c
Bohdan Milenko <bogdan57milenko@gmail.com>
2020-03-13 20:08:12 -0400

    Updated connection link to Mongo for Docker

--- src/com/musicLib/repository/mongoUtil/SessionManagerMongo.java
+++ src/com/musicLib/repository/mongoUtil/SessionManagerMongo.java
@@ -1,12 +1,12 @@
 package com.musicLib.repository.mongoUtil;
 
-import com.mongodb.*;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
 import com.mongodb.client.MongoDatabase;
 import com.musicLib.exceptions.DbConnectionException;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
@@ -18,16 +18,28 @@ public class SessionManagerMongo {
     private static final String mongoPortExternalProperty = "externalPort";
     private static final String mongoUserNameProperty = "MONGO_INITDB_ROOT_USERNAME";
     private static final String mongoPasswordProperty = "MONGO_INITDB_ROOT_PASSWORD";
+    //    private static final MongoCredential credentials = MongoCredential.createCredential(mongoUser, databaseName, mongoPassword);
+//    private static final MongoClient mongoClient = new MongoClient(new ServerAddress(ip, externalPort), Arrays.asList(credentials));
 
     private static Map<String, String> properties;
+    private static final String databaseName = properties.get(databaseNameProperty);
+    private static final String ip = properties.get(dockerIpProperty);
+    private static final int externalPort = Integer.parseInt(properties.get(mongoPortExternalProperty));
+    private static final String mongoUser = properties.get(mongoUserNameProperty);
+    private static final char[] mongoPassword = properties.get(mongoPasswordProperty).toCharArray();
+    private static final String mongoClientURI = "mongodb://" + mongoUser + ":" + mongoPassword + "@" + ip + ":" + externalPort + "/" + databaseName;
+    private static final MongoClient mongoClient = new MongoClient(new MongoClientURI(mongoClientURI));
 
     static {
         try {
+            System.out.println(mongoClientURI);
             properties = loadMongoProperties();
+
         } catch (DbConnectionException e) {
             e.printStackTrace();
         }
     }
+
 
     private static Map<String, String> loadMongoProperties() throws DbConnectionException {
         Map<String, String> returnMap = new HashMap<>();
@@ -39,7 +51,7 @@ public class SessionManagerMongo {
             returnMap.put(mongoPortExternalProperty, p.getProperty(mongoPortExternalProperty));
             returnMap.put(mongoUserNameProperty, p.getProperty(mongoUserNameProperty));
             returnMap.put(mongoPasswordProperty, p.getProperty(mongoPasswordProperty));
-            returnMap.forEach((k,v) -> System.out.println(k + ": " + v));
+            returnMap.forEach((k, v) -> System.out.println(k + ": " + v));
             return returnMap;
         } catch (IOException e) {
             System.out.println(e.getMessage() + ": Cannot load Mongo Properties");
@@ -47,15 +59,6 @@ public class SessionManagerMongo {
             throw new DbConnectionException("Unable to connect to MongoDB", e);
         }
     }
-
-    private static final String databaseName = properties.get(databaseNameProperty);
-    private static final String ip = properties.get(dockerIpProperty);
-    private static final int externalPort = Integer.parseInt(properties.get(mongoPortExternalProperty));
-    private static final String mongoUser = properties.get(mongoUserNameProperty);
-    private static final char[] mongoPassword = properties.get(mongoPasswordProperty).toCharArray();
-
-    private static final MongoCredential credentials = MongoCredential.createCredential(mongoUser, databaseName, mongoPassword);
-    private static final MongoClient mongoClient = new MongoClient(new ServerAddress(ip, externalPort), Arrays.asList(credentials));
 
     public static MongoClient getMongoClient() {
         return mongoClient;
