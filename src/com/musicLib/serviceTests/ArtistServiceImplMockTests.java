e4e7df2d47e77d622bf760b94ce87efa2ed651cd
Bohdan Milenko <bogdan57milenko@gmail.com>
2020-03-15 00:57:55 -0400

    New Mock tests added

--- src/com/musicLib/serviceTests/ArtistServiceImplMockTests.java
+++ src/com/musicLib/serviceTests/ArtistServiceImplMockTests.java
@@ -1,6 +1,7 @@
 package com.musicLib.serviceTests;
 
 import com.musicLib.entities.Artist;
+import com.musicLib.exceptions.QueryException;
 import com.musicLib.exceptions.ServiceException;
 import com.musicLib.repository.AlbumRepository;
 import com.musicLib.repository.ArtistRepository;
@@ -9,13 +10,14 @@ import com.musicLib.repository.MongoDBRepisotory.ArtistRepositoryMongo;
 import com.musicLib.repository.MongoDBRepisotory.SongRepositoryMongo;
 import com.musicLib.repository.SongRepository;
 import com.musicLib.services.*;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -26,12 +28,55 @@ public class ArtistServiceImplMockTests {
     //ArtistServiceImp - SUT System under Test
     //ArtistRepo - is a dependency
     SongService songServiceMongo;
-    AlbumService albumServiceMongo;
+    AlbumService albumService;
     ArtistRepository artistRepositoryStub = new ArtistRepositoryMongoTestStub();
     ArtistService artistServiceMongoStubBased = new ArtistServiceImpl(artistRepositoryStub);
 
 
     private ArtistRepository artistRepositoryMock;
+    private RecordValidator recordValidator;
+    private ArtistService artistServiceMock;
+    private Artist artist;
+
+    @BeforeEach
+    public void setUp() {
+        artistRepositoryMock = mock(ArtistRepository.class);
+        recordValidator = mock(RecordValidator.class);
+        artistServiceMock = new ArtistServiceImpl(artistRepositoryMock);
+        artistServiceMock.setRecordValidator(recordValidator);
+        albumService = mock(AlbumService.class);
+        artistServiceMock.setAlbumService(albumService);
+
+        artist = new Artist();
+        artist.setName("Bob Dylan");
+    }
+
+    @Test
+    public void testAddValidationPassedObjectAddedToDB() throws ServiceException {
+        when(recordValidator.validateArtistAddMethod(artist)).thenReturn(true);
+        when(artistServiceMock.add(artist)).thenReturn(true);
+        assertTrue(artistServiceMock.add(artist));
+    }
+
+    @Test
+    public void testAddValidationNotPassed() throws ServiceException {
+        when(recordValidator.validateArtistAddMethod(artist)).thenThrow(ServiceException.class);
+        assertThrows(ServiceException.class, () -> artistServiceMock.add(artist));
+    }
+
+    @Test
+    public void testAddValidationPassedObjectNotAddedToDB() throws ServiceException {
+        when(recordValidator.validateArtistAddMethod(artist)).thenReturn(true);
+        when(artistServiceMock.add(artist)).thenThrow(QueryException.class);
+        assertThrows(ServiceException.class, () -> artistServiceMock.add(artist));
+    }
+
+    @Test
+    public void testAddValidationPassedObjectNotAddedToDBSQL() throws ServiceException {
+        when(recordValidator.validateArtistAddMethod(artist)).thenReturn(true);
+        when(artistServiceMock.add(artist)).thenThrow(SQLException.class);
+        assertThrows(ServiceException.class, () -> artistServiceMock.add(artist));
+    }
 
     @Test
     public void testGetAllMethodUsingStub() throws ServiceException {
@@ -41,44 +86,52 @@ public class ArtistServiceImplMockTests {
 
     @Test
     public void testGetAllUsingMock() throws ServiceException {
-        artistRepositoryMock = mock(ArtistRepository.class);
-        ArtistService artistServiceMongoMock = new ArtistServiceImpl(artistRepositoryMock);
-        when(artistServiceMongoMock.getAll()).thenReturn(Arrays.asList(new Artist(), new Artist()));
-        List<Artist> returnedArtists = artistServiceMongoMock.getAll();
+        when(artistServiceMock.getAll()).thenReturn(Arrays.asList(new Artist(), new Artist()));
+        List<Artist> returnedArtists = artistServiceMock.getAll();
         assertEquals(2, returnedArtists.size());
+        assertEquals(returnedArtists.get(0).getClass(), Artist.class);
     }
 
     @Test
-    public void testGetByNameUsingMocks() throws ServiceException {
-        artistRepositoryMock = mock(ArtistRepository.class);
-        RecordValidator recordValidator = mock(RecordValidator.class);
-        ArtistService artistServiceMongoMock = new ArtistServiceImpl(artistRepositoryMock);
-        artistServiceMongoMock.setRecordValidator(recordValidator);
-
-        Artist artist = new Artist();
-        artist.setName("Bob");
-
+    public void testGetAllValidationPassedRepoFailedSQL() throws ServiceException {
         when(recordValidator.validateIfNotNull(any())).thenReturn(true);
-        when(artistServiceMongoMock.getByName(artist)).thenReturn(Arrays.asList(new Artist(), new Artist()));
-
-        List<Artist> returnedArtists = artistServiceMongoMock.getByName(artist);
-        assertEquals(2, returnedArtists.size());
+        when(artistServiceMock.getAll()).thenThrow(SQLException.class);
+        assertThrows(ServiceException.class, () -> artistServiceMock.getAll());
     }
 
     @Test
-    public void testGetByNameUsingMocks2() throws ServiceException {
-        artistRepositoryMock = mock(ArtistRepository.class);
-        RecordValidator recordValidator = mock(RecordValidator.class);
-        ArtistService artistServiceMongoMock = new ArtistServiceImpl(artistRepositoryMock);
-        artistServiceMongoMock.setRecordValidator(recordValidator);
+    public void testGetByNameValidationPassedRepoWorkedAsExpected() throws ServiceException {
+        when(recordValidator.validateIfNotNull(any())).thenReturn(true);
+        when(artistServiceMock.getByName(artist)).thenReturn(Arrays.asList(new Artist(), new Artist()));
+        assertEquals(2, artistServiceMock.getByName(artist).size());
+    }
 
-        Artist artist = new Artist();
-        artist.setName("Bob");
+    @Test
+    public void testGetByNameValidationNotPassed() throws ServiceException {
+        when(recordValidator.validateIfNotNull(any())).thenThrow(ServiceException.class);
+        assertThrows(ServiceException.class, () -> artistServiceMock.getByName(artist));
+    }
 
-        when(recordValidator.validateIfNotNull(any())).thenThrow(new ServiceException());
-        when(artistServiceMongoMock.getByName(artist)).thenReturn(Arrays.asList(new Artist(), new Artist()));
+    @Test
+    public void testGetByNameValidationPassedRepoFailed() throws ServiceException {
+        when(recordValidator.validateIfNotNull(any())).thenReturn(true);
+        when(artistServiceMock.getByName(artist)).thenThrow(SQLException.class);
+        assertThrows(ServiceException.class, () -> artistServiceMock.getByName(artist));
+    }
 
-        assertThrows(ServiceException.class,  () -> artistServiceMongoMock.getByName(artist));
+    @Test
+    public void testDeleteValidationPassedRepoWorkedAsExpected() throws ServiceException, SQLException {
+        when(recordValidator.validateArtistDeleteMethod(artist)).thenReturn(true);
+        when(recordValidator.hasDependantAlbums(artist)).thenReturn(true);
+        //Ask how to test if it returns void, should I change the return type here?
+        //when(albumService.deleteAlbumsForArtist(artist)).thenThrow(ServiceException.class);
+        assertTrue(artistServiceMock.delete(artist));
+    }
+
+    @Test
+    public void testDeleteValidationPassedRepoWorkedAsExpected2() throws ServiceException {
+        when(recordValidator.validateArtistDeleteMethod(any())).thenThrow(ServiceException.class);
+        assertThrows(ServiceException.class, () -> artistServiceMock.delete(artist));
     }
 
 
@@ -88,20 +141,20 @@ public class ArtistServiceImplMockTests {
         SongRepository songRepositoryMongo = new SongRepositoryMongo();
 
         songServiceMongo = new SongServiceImpl(songRepositoryMongo);
-        albumServiceMongo = new AlbumServiceImpl(albumRepositoryMongo);
+        albumService = new AlbumServiceImpl(albumRepositoryMongo);
         artistServiceMongoStubBased = new ArtistServiceImpl(artistRepositoryMongo);
 
-        RecordValidator recordValidator = new RecordValidator(artistServiceMongoStubBased, albumServiceMongo, songServiceMongo);
+        RecordValidator recordValidator = new RecordValidator(artistServiceMongoStubBased, albumService, songServiceMongo);
 
-        songServiceMongo.setAlbumService(albumServiceMongo);
+        songServiceMongo.setAlbumService(albumService);
         songServiceMongo.setRecordValidator(recordValidator);
 
-        artistServiceMongoStubBased.setAlbumService(albumServiceMongo);
+        artistServiceMongoStubBased.setAlbumService(albumService);
         artistServiceMongoStubBased.setRecordValidator(recordValidator);
 
-        albumServiceMongo.setSongService(songServiceMongo);
-        albumServiceMongo.setArtistService(artistServiceMongoStubBased);
-        albumServiceMongo.setRecordValidator(recordValidator);
+        albumService.setSongService(songServiceMongo);
+        albumService.setArtistService(artistServiceMongoStubBased);
+        albumService.setRecordValidator(recordValidator);
 
         return artistServiceMongoStubBased;
     }
