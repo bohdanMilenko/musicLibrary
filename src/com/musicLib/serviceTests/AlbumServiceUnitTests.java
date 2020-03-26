ea242e05a4badd731b50dee8785c32eef9119b5f
Bohdan Milenko <bogdan57milenko@gmail.com>
2020-03-25 21:55:05 -0400

    Tested three more methods

--- src/com/musicLib/serviceTests/AlbumServiceUnitTests.java
+++ src/com/musicLib/serviceTests/AlbumServiceUnitTests.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -42,11 +43,13 @@ public class AlbumServiceUnitTests {
         album.setArtist(artist);
     }
 
+
+    //TESTING add(Album album)
     @Test
     public void testAddMethodWorkedAsExpected() throws ServiceException, SQLException {
         //Defining expected results for AlbumService: updateAlbumWithArtistID(album)
         when(artistServiceMock.getByName(any())).thenReturn(Arrays.asList(artist, artist));
-        when(artist.getId()).thenReturn(52);
+        when(artist.getId()).thenReturn(anyInt());
         when(album.getArtist()).thenReturn(artist);
         //Service method results defining
         when(recordValidatorMock.validateAlbumAddMethod(album)).thenReturn(true);
@@ -61,7 +64,7 @@ public class AlbumServiceUnitTests {
     public void testAddValidationFailed() throws ServiceException, SQLException {
         //Defining expected results for AlbumService: updateAlbumWithArtistID(album)
         when(artistServiceMock.getByName(any())).thenReturn(Arrays.asList(artist, artist));
-        when(artist.getId()).thenReturn(52);
+        when(artist.getId()).thenReturn(anyInt());
         when(album.getArtist()).thenReturn(artist);
         //Service method results defining
         when(recordValidatorMock.validateAlbumAddMethod(album)).thenThrow(ServiceException.class);
@@ -74,7 +77,7 @@ public class AlbumServiceUnitTests {
     public void testAddValidationPassedRepoFailed() throws ServiceException, SQLException {
         //Defining expected results for AlbumService: updateAlbumWithArtistID(album)
         when(artistServiceMock.getByName(any())).thenReturn(Arrays.asList(artist, artist));
-        when(artist.getId()).thenReturn(52);
+        when(artist.getId()).thenReturn(anyInt());
         when(album.getArtist()).thenReturn(artist);
         //Service method results defining
         when(recordValidatorMock.validateAlbumAddMethod(album)).thenReturn(true);
@@ -97,18 +100,81 @@ public class AlbumServiceUnitTests {
     }
 
 
+    //TESTING get(Album album)
     @Test
     public void testGetWorkedAsExpected() throws ServiceException, SQLException {
         when(recordValidatorMock.validateRecordForNulls(album)).thenReturn(true);
-        when(albumRepoMock.getByName(any())).thenReturn(Arrays.asList(album,album));
-        assertEquals( 2 ,classToTest.get(album).size());
+        when(albumRepoMock.getByName(any())).thenReturn(Arrays.asList(new Album(), new Album()));
+        assertEquals(2, classToTest.get(album).size());
+        assertEquals(Album.class, classToTest.get(album).get(0).getClass());
     }
 
     //Create ValidationException and then catch it and wrap it as ServiceException?
     @Test
     public void testGetValidationFailed() throws ServiceException {
-        when(recordValidatorMock.validateRecordForNulls(album)).thenThrow(ServiceException.class);
+        when(recordValidatorMock.validateRecordForNulls(any(Album.class))).thenThrow(ServiceException.class);
         ServiceException e = assertThrows(ServiceException.class, () -> classToTest.get(album));
-        assertEquals("Validation for nulls failed", e.getMessage());
+        //assertEquals("Validation for nulls failed", e.getMessage());
+        assertEquals(null, e.getMessage());
+    }
+
+    @Test
+    public void testGetValidationPassedRepoFailed() throws ServiceException, SQLException {
+        when(recordValidatorMock.validateRecordForNulls(any(Album.class))).thenReturn(true);
+        when(albumRepoMock.getByName(any())).thenThrow(SQLException.class);
+        ServiceException e = assertThrows(ServiceException.class, () -> classToTest.get(album));
+        assertEquals("Issue with db connectivity", e.getMessage());
+    }
+
+
+    //TESTING getByArtist(Artists artist)
+    @Test
+    void testGetByArtistWorkedAsExpected() throws ServiceException, SQLException {
+        //Setting up the behavior
+        when(artist.getId()).thenReturn(anyInt());
+        when(recordValidatorMock.validateGetAlbumByArtist(artist)).thenReturn(true);
+        when(artistServiceMock.updateArtistID(artist)).thenReturn(artist);
+        when(albumRepoMock.getAlbumsByArtistID(anyInt())).thenReturn(Arrays.asList(new Album(), new Album()));
+        //Actual Test
+        List<Album> returnedAlbums = classToTest.getByArtist(artist);
+        assertEquals(2, returnedAlbums.size());
+        assertEquals(Album.class, returnedAlbums.get(0).getClass());
+    }
+
+    @Test
+    void testGetByArtistValidationFailed() throws ServiceException, SQLException {
+        //Setting up the behavior
+        when(recordValidatorMock.validateGetAlbumByArtist(any(Artist.class))).thenThrow(new ServiceException());
+        //Actual Test
+        ServiceException e = assertThrows(ServiceException.class, () -> classToTest.getByArtist(artist));
+        assertNull(e.getMessage());
+    }
+
+    @Test
+    void testGetByArtistValidationPassedUpdateFailed() throws ServiceException, SQLException {
+        //Setting up the behavior
+        when(recordValidatorMock.validateGetAlbumByArtist(artist)).thenReturn(true);
+        when(artistServiceMock.updateArtistID(any(Artist.class))).thenThrow(ServiceException.class);
+        //Actual Test
+        ServiceException e = assertThrows(ServiceException.class, () -> classToTest.getByArtist(artist));
+        assertNull(e.getMessage());
+    }
+
+    @Test
+    void testGetByArtistRepoFailed() throws ServiceException, SQLException {
+        //Setting up the behavior
+        when(artist.getId()).thenReturn(anyInt());
+        when(recordValidatorMock.validateGetAlbumByArtist(artist)).thenReturn(true);
+        when(artistServiceMock.updateArtistID(artist)).thenReturn(artist);
+        when(albumRepoMock.getAlbumsByArtistID(anyInt())).thenThrow(SQLException.class);
+        //Actual Test
+        ServiceException e = assertThrows(ServiceException.class, () -> classToTest.getByArtist(artist));
+        assertEquals("Unable to get albums by artist:\n\t" + artist.toString() ,e.getMessage());
+    }
+
+    //TESTING Delete Method
+    @Test
+    void testDelete() throws ServiceException, SQLException {
+
     }
 }
